6a2893cd2f5de5e5071490ad19a86af271db08fd
Katy7711 <deniev777@yandex.ru>
2022-09-29 14:44:13 +0500

    Homework arrays part 2

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,68 @@
 public class Main {
     public static void main(String[] args) {
-        tusk_1_2();
-        tusk_3();
-        tusk_4();
-    }
 
-    public static void tusk_1_2() {
         int[] weight = new int[3];
         weight[0] = 1;
         weight[1] = 2;
         weight[2] = 3;
-        System.out.println(weight[0] + "," + weight[1] + "," + weight[2]);
-        float[] numbers = {1.57f, 7.654f, 9.986f};
-        System.out.println(numbers[0] + " , " + numbers[1] + " , " + numbers[2]);
-        int[] cat = new int[5];
-        cat[0] = 2;
-        cat[1] = 6;
-        cat[2] = 7;
-        cat[3] = 2;
-        cat[4] = 9;
-        System.out.println(cat[0] + "," + cat[1] + "," + cat[2] + "," + cat[3] + "," + cat[4]);
-    }
+        double[] numbers = {1.57f, 7.654f, 9.986f};
+        int[] cat = {1, 2, 3, 4, 5};
 
-    public static void tusk_3() {
-        int[] weight = new int[3];
-        weight[0] = 1;
-        weight[1] = 2;
-        weight[2] = 3;
-        System.out.println(weight[2] + "," + weight[1] + "," + weight[0]);
-        float[] numbers = {1.57f, 7.654f, 9.986f};
-        System.out.println(numbers[2] + " , " + numbers[1] + " , " + numbers[0]);
-        int[] cat = new int[5];
-        cat[0] = 2;
-        cat[1] = 6;
-        cat[2] = 7;
-        cat[3] = 2;
-        cat[4] = 9;
-        System.out.println(cat[4] + "," + cat[3] + "," + cat[2] + "," + cat[1] + "," + cat[0]);
-    }
+        for (int i = 0; i < weight.length; i++) {
+            if (i != weight.length - 1) {
+                System.out.print(weight[i] + " ");
+            } else {
+                System.out.println(weight[i]);
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (i != numbers.length - 1) {
+                System.out.print(numbers[i] + " ");
+            } else {
+                System.out.println(numbers[i]);
+            }
+        }
+        for (int i = 0; i < cat.length; i++) {
+            if (i != cat.length - 1) {
+                System.out.print(cat[i] + " ");
+            } else {
+                System.out.println(cat[i]);
+            }
+        }
 
-    public static void tusk_4() {
-        int[] weight = {1, 2, 3};
-        for (int j : weight) {
+
+        for (int i = weight.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(weight[i] + " ");
+            } else {
+                System.out.println(weight[i]);
+            }
+        }
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(numbers[i] + " ");
+            } else {
+                System.out.println(numbers[i]);
+            }
+        }
+        for (int i = cat.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(cat[i] + " ");
+            } else {
+                System.out.println(cat[i]);
+            }
+        }
+
+
+        int[] weights = {1, 2, 3};
+        for (int j : weights) {
             if (j % 2 != 0) {
                 System.out.println(j + 1);
             } else {
                 System.out.println(j);
+
+
             }
         }
     }
 }
-
-
-
-
-
-        
-
-
